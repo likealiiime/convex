c189a492097e10b8f01b27bcdf3873898b6e874b
Sherrod Faulks <sherrod@softilluminations.com>
2010-07-04 19:30:33 -0400

    Move Convex::Environment, work with testing env, and use engine codes

--- convex.rb
+++ convex.rb
@@ -5,43 +5,40 @@ require 'ruby-debug'
 require 'pp'
 
 require 'extensions'
+
+require 'engine'
 require 'datum_type'
 require 'datum'
-
-module Convex
-  class Environment
-    attr_accessor :stage
-    def initialize(stage)
-      @stage = stage.to_sym
-    end
-    def code
-      case stage.to_s
-      when 'development' then 0
-      else 1
-      end
-    end
-    def development?; stage == :development; end
-  end
-end
    
 module Convex
-  def self.db; @@db; end
+  @@next_engine_code = nil
   def self.env; @@env; end
+  def self.db; @@db; end
   
   def self.boot!(stage = :development)
     @@env = Convex::Environment.new(stage)
+    @@filters = []
     @@db = Redis.new
     debug "Connected to Redis"
     @@db.select env.code
-    debug "Selecting #{env.stage} environment, code #{env.code}"
+    debug "Selected #{env.stage} environment, code #{env.code}"
     @@db.setnx '_lachesis', 0
+    if env.testing?
+      debug "Performing FLUSHDB"
+      @@db.flushdb
+    end
     
     debug "Booting..."
     DatumType.load!
+    debug "Loaded DatumTypes"
+  end
+  
+  def self.next_engine_code
+    @@next_engine_code = @@next_engine_code.nil? ? 'A' : @@next_engine_code.succ
   end
   
   def self.debug(message)
-    puts "--- " << message if env.development?
+    puts "--- " << message.to_s if env.development?
   end
   
   def self.nid
@@ -52,6 +49,20 @@ module Convex
   end
   def self.cidi
     Convex.db.get('_lachesis').to_i
+  end
+end
+
+module Convex
+  class Environment
+    attr_accessor :stage
+    def initialize(stage)
+      @stage = stage.to_sym
+    end
+    def code
+      [:development, :testing].index(stage)
+    end
+    def development?; stage == :development || stage == :testing; end
+    def testing?; stage == :testing; end
   end
 end
 
