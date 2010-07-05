9498d8df668454acfd4da012a907323820654beb
Sherrod Faulks <sherrod@softilluminations.com>
2010-07-04 22:07:01 -0400

    Restructure files

--- convex.rb
+++ convex.rb
@@ -1,76 +1,24 @@
-require 'rubygems'
-require 'redis'
-require 'nokogiri'
-require 'ruby-debug'
-require 'pp'
+require 'lib/convex'
 
-require 'logging'
-require 'extensions'
-require 'calais_service'
-require 'engine'
-require 'datum_type'
-require 'datum'
-   
-module Convex
-  extend Convex::Logging
-  
-  @@next_engine_code = nil
-  def self.env; @@env; end
-  def self.db; @@db; end
-  
-  def self.boot!(mode = :development)
-    @@env = Convex::Environment.new(mode)
-    
-    Convex.info "Starting Convex in #{env.mode.to_s.upcase} mode..."
-    @@db = Redis.new
-    debug "Connected to Redis"
-    @@db.select env.code
-    debug "SELECTed #{env.mode} database, code #{env.code}"
-    @@db.setnx '_lachesis', 0
-    if env.forgetful?
-      warn "Performing FLUSHDB"
-      @@db.flushdb
-    end
-    
-    debug "Booting..."
-    Convex::DatumType.load!
-    info "Loaded DatumTypes"
-    log_newline
-  end
-  
-  def self.next_engine_code
-    @@next_engine_code = @@next_engine_code.nil? ? 'A' : @@next_engine_code.succ
-  end
-  
-  def self.nid
-    Convex.db.incr('_lachesis').to_i
-  end
-  def self.cid
-    Convex.db.get '_lachesis'
-  end
-  def self.cidi
-    Convex.db.get('_lachesis').to_i
-  end
-end
+Convex.boot! :forgetful
+EngineA = Convex::Engine.new
+EngineA.focus! DATA.read
 
-module Convex
-  class Environment
-    attr_accessor :mode
-    def initialize(mode)
-      @mode = mode.to_sym
-    end
-    def code
-      [:development, :forgetful].index(mode)
-    end
-    def development?; mode == :development || mode == :forgetful; end
-    def forgetful?; mode == :forgetful; end
-  end
-end
+__END__
+There is almost nothing more magical than a spectacular fireworks show – and now is the perfect time to let those sparklers in the sky serve as inspiration for your fingertips.
+While some people–like the fabulous girls at WAH Nails (http://wah-nails.com/) in London–know how to get their nails properly did, many of us opt for simple petal-pink or deep red manis. Break out of your boring nail routine by adding a bit of sparkle to your hands via light-catching nail polish and blinding bling.
 
-names = <<REDIS
-lens-type->id attribute
+For something really outrageous, slip on Bijules gold nail rings (http://styledon.com/products/bijules/647-gold-nail-ring) (shown above left). Capping your nails in gold and securing them with a sexy serpent, these bad boys go for $160 a pop. For a less expensive gilded option, go with Chanel's Illusion D'Or (http://styledon.com/products/chanel/1387-illusion-dor) - I tried it the other day at Jin Soon Salon & Spa and it's gorgeous!! Play with this one as a French cap or use it to make a sparkling strip over another, brighter color. Bring more attention to hand with Iosselliani's gold and rhinestone ring, $312.
 
-chaos-datum->sdfdf0sdfsdofim value
-phanes-datum->asdofmsodifmsd value
-chronos-datum->asdfasdfsolmd value
-REDIS
+If you've never tried Deborah Lippmann nail products, you simply must. Her Smooth Operator buffer, $12, is the best we've ever used ever and her polishes brush on beautifully. Get the party started with glittery HAPPY BIRTHDAY and Marquee Moon lacquers, $16-18.
+
+We introduced you to DANNIJO jewels earlier this year and now we're obsessed with their Aris ring, $215, which covers two fingers due to its three crystal clutters. Want the bang for less buck? Try the blue cocktail ring above for $65.If you can, we highly suggest adding bling to your bare hands in the form of killer Dior Fine Jewelry. As you can see via the skull and 'Oui' solitaire rings above and the pavé garden ring below, Dior Fine Jewelry is simply beyond. Love the look, but not in the position to make a purchase? Slip on Juicy Couture's Floral Cluster Ring, $88, instead. Shown above, far right, this ring is a bright bouquet of girly fun on your finger.
+
+Sephora by OPI is always a reliable source for fun, crazy colors - Midnight Mambo and Teal We Meet Again, $9, are hitting the spot for us today. Vibrant, young and fun, we can't get enough of them.
+
+
+Honestly, not much can rival the 'wow' factor of Minx nails, and now Sephora by OPI has teamed up with Minx to bring us a line called Chic Prints for Nails, you can do the mad manis in the comfort of your own home for $15 a pack.
+
+For a vivid color that dries faster than a small fireworks fountain, reach for a bottle of Sally Hansen Insta-Dri in Pronto Purple. At just $3 a bottle, you now have no reason to show up to a party this weekend with bare nails. Rather go platinum? Pick up a bottle of Zoya in Trixie, $7. Of course, you could always look be inspired by WAH and nail it with heavy rhinestone coverage, a la the picture seen above, far right.
+
+Looking for more great costume jewelry? Shop a ton of big blingy rings here...
---
+++ lib/convex.rb
@@ -0,0 +1,83 @@
+# Ruby libraries
+require 'cgi'
+require 'net/http'
+require 'uri'
+require 'pp'
+
+# 3rd-party gems
+require 'rubygems'
+require 'ruby-debug'
+require 'redis'
+require 'nokogiri'
+
+# Convex libraries
+require 'lib/logging'
+require 'lib/extensions'
+require 'lib/calais_service'
+require 'lib/engine'
+require 'lib/datum_type'
+require 'lib/datum'
+   
+module Convex
+  extend Convex::Logging
+  
+  @@next_engine_code = nil
+  def self.env; @@env; end
+  def self.db; @@db; end
+  
+  def self.boot!(mode = :development)
+    @@env = Convex::Environment.new(mode)
+    
+    Convex.info "Starting Convex in #{env.mode.to_s.upcase} mode..."
+    @@db = Redis.new
+    debug "Connected to Redis"
+    @@db.select env.code
+    debug "SELECTed #{env.mode} database, code #{env.code}"
+    @@db.setnx '_lachesis', 0
+    if env.forgetful?
+      warn "Performing FLUSHDB"
+      @@db.flushdb
+    end
+    
+    debug "Booting..."
+    Convex::DatumType.load!
+    info "Loaded DatumTypes"
+    log_newline
+  end
+  
+  def self.next_engine_code
+    @@next_engine_code = @@next_engine_code.nil? ? 'A' : @@next_engine_code.succ
+  end
+  
+  def self.nid
+    Convex.db.incr('_lachesis').to_i
+  end
+  def self.cid
+    Convex.db.get '_lachesis'
+  end
+  def self.cidi
+    Convex.db.get('_lachesis').to_i
+  end
+end
+
+module Convex
+  class Environment
+    attr_accessor :mode
+    def initialize(mode)
+      @mode = mode.to_sym
+    end
+    def code
+      [:development, :forgetful].index(mode)
+    end
+    def development?; mode == :development || mode == :forgetful; end
+    def forgetful?; mode == :forgetful; end
+  end
+end
+
+names = <<REDIS
+lens-type->id attribute
+
+chaos-datum->sdfdf0sdfsdofim value
+phanes-datum->asdofmsodifmsd value
+chronos-datum->asdfasdfsolmd value
+REDIS
