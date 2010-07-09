598a1d2626d4310c4dd7f832f6eb025e7ae42ac7
StyledOn <dev@styledon.com>
2010-07-09 11:52:20 -0400

    Don't send back anything anymore. Refining is on hold for now.

--- services/convex_service.rb
+++ services/convex_service.rb
@@ -19,24 +19,13 @@ module Convex
     def receive_data(json)
       Convex::ConvexFocusingService.info("Received %.1fKB of data" % (json.size / 1024.0))
       transport = JSON.parse(json)
-      puts json
-      pp transport
       data = Convex::Engine.new.focus! transport['document'].to_s, transport['data']
-      send_data JSON.generate(data)
-      close_connection_after_writing
+      close_connection
     end
     
     def unbind
       Convex::ConvexFocusingService.info "Closed connection"
     end
-  end
-  
-  module ConvexRefiningService
-    extend Convex::CustomizedLogging
-    PORT = 7334 # = REFI
-    
-    def self.log_preamble; 'ConvexRefiningService'; end
-    
   end
 end
 
@@ -45,5 +34,4 @@ EventMachine::run do
   Convex << Convex::Lenses::ChronosLens
   Convex.info "Now listening for incoming connections on 127.0.0.1:2689..."
   EventMachine::start_server Convex::SERVICE_ADDRESS, Convex::ConvexFocusingService::PORT, Convex::ConvexFocusingService
-  EventMachine::start_server Convex::SERVICE_ADDRESS, Convex::ConvexRefiningService::PORT, Convex::ConvexRefiningService 
 end
