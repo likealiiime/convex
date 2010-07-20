7e8719c707706ffd246703182d116edfee5d8bf3
StyledOn <dev@styledon.com>
2010-07-20 12:12:57 -0400

    Add Eros Lens

---
+++ lenses/eros/lens.rb
@@ -0,0 +1,13 @@
+module Convex
+  module Eros
+    module Lens
+      extend Convex::CustomizedLogging
+      
+      def self.focus_using_data!(data, engine)
+        log_newline
+        
+      end
+      
+    end
+  end
+end
