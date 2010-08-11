65ec588883fcff559c0a8d0dc3e02ba020657c85
StyledOn <dev@styledon.com>
2010-08-11 19:07:06 -0400

    Add Sec- Headers

--- lib/em-websocket/em-websocket/handler75.rb
+++ lib/em-websocket/em-websocket/handler75.rb
@@ -9,7 +9,9 @@ module EventMachine
         upgrade =  "HTTP/1.1 101 Web Socket Protocol Handshake\r\n"
         upgrade << "Upgrade: WebSocket\r\n"
         upgrade << "Connection: Upgrade\r\n"
+        upgrade << "Sec-WebSocket-Origin: #{@request['Origin']}\r\n"
         upgrade << "WebSocket-Origin: #{@request['Origin']}\r\n"
+        upgrade << "Sec-WebSocket-Location: #{location}\r\n"
         upgrade << "WebSocket-Location: #{location}\r\n\r\n"
 
         debug [:upgrade_headers, upgrade]
