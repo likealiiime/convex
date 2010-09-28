e775cc5959703afd2d63847222045cb1043b0f5a
StyledOn <dev@styledon.com>
2010-09-28 14:49:41 -0400

    Use the Command classes

--- web/web.rb
+++ web/web.rb
@@ -4,24 +4,17 @@ require 'sinatra/reloader'
 
 module Convex
   class Web < Sinatra::Base
-    class Service < Struct.new(:name, :command)
-      def stop_command
-        "#{File.join(Convex::ROOT_PATH, self.command)} stop"
-      end
-    end
-    Services = {
-      :Convex => Convex::Web::Service.new(:Convex, 'convexd')
-    }
-    
     set :root, File.dirname(__FILE__)
     
     configure :development do
       register Sinatra::Reloader
-      require File.join(File.dirname(__FILE__), '..', 'lib', 'convex')
+      require File.expand_path(File.join(File.dirname(__FILE__), '..', 'lib', 'convex'))
       require 'ruby-debug'
+      Convex::RedisCommand.conf = File.expand_path(File.join(File.dirname(__FILE__), '..', 'redis.conf'))
     end
     configure :production do
       require 'convex'
+      Convex::RedisCommand.conf = '/etc/redis/redis.conf'
     end
     configure do
       Convex.headless!
@@ -38,15 +31,15 @@ module Convex
         {
           :name => :Convex,
           :address => "#{Convex::Service::ADDRESS}:#{Convex::ConvexFocusingService::PORT}",
-          :running => File.exist?(File.join(Convex::PIDS_PATH, 'convexd.pid'))
+          :running => Convex::ConvexCommand.running?
         },{
           :name => :Chronos,
           :address => "#{Convex::Service::ADDRESS}:#{Convex::Chronos::Service::PORT}",
-          :running => File.exist?(File.join(Convex::PIDS_PATH, 'chronosd.pid'))
+          :running => Convex::ChronosCommand.running?
         },{
           :name => :Redis,
           :address => "#{Convex::Service::ADDRESS}:#{Convex::RedisService::PORT}",
-          :running => File.exist?(File.join(Convex::PIDS_PATH, 'redis.pid'))
+          :running => Convex::RedisCommand.running?
         }
       ]
       erubis :index
@@ -54,8 +47,7 @@ module Convex
     
     post '/service/:name/stop' do |name|
       name = name.to_sym
-      command = Convex::Web::Services[name].stop_command
-      puts command
+      result = Convex::Command[name].stop!
     end
   end
 end
