e7ec4c7f21bd6f4f1a3e575a6bbd3f2bef3edfe9
StyledOn <dev@styledon.com>
2010-11-28 18:53:41 -0500

    Greatly simplify -- all logging goes to ~/.convex/log now

--- lib/environment.rb
+++ lib/environment.rb
@@ -10,34 +10,20 @@ module Convex
       raise EnvironmentNotRecognizedError.new("#{mode} is not one of: #{MODES.join(', ')}") unless MODES.include? mode.to_sym
       @mode = mode.to_sym
       
-      # Set up paths
-      local_paths = if production?
-        Convex.const_set('LOG_PATH',  Convex::Environment.log_dir_for_mode(mode).freeze)
-        FileUtils.mkdir_p Convex.const_set('TMP_PATH',  '/tmp/convex'.freeze)
-        FileUtils.mkdir_p Convex.const_set('PIDS_PATH', File.expand_path('~/.convex/run').freeze)
-        []
-      elsif headless?
-        Convex.const_set('LOG_PATH', Convex::Environment.log_dir_for_mode(mode).freeze)
-        %w(tmp pid)
+      # Set up TMP_PATH because headless mode doesn't save temporary files
+      if headless?
+        Convex.const_set('TMP_PATH', '/dev/null'.freeze)
       else
-        %w(log tmp pid)
+        FileUtils.mkdir_p Convex.const_set('TMP_PATH',  '/tmp/convex'.freeze)
       end
       
-      local_paths.each do |path|
-        Convex.const_set("#{path.upcase}_PATH", File.join(ROOT_PATH, path).freeze)
-        FileUtils.mkdir_p Convex.const_get("#{path.upcase}_PATH")
-      end
       # Set up logging
+      Convex.const_set('LOG_PATH', Convex::Environment::log_dir_for_mode(mode))
       Convex::Logging.open_log_at File.join(Convex::LOG_PATH, "#{mode}.log")
     end
     
     def self.log_dir_for_mode(mode = :forgetful)
-      case mode.to_sym
-      when :production, :headless
-        return File.expand_path('~/.convex/log')
-      else
-        return 'log'
-      end
+      File.expand_path('~/.convex/log')
     end
     
     def code; mode == :headless ? -1 : MODES.index(mode); end
